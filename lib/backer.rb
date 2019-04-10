7c05ed53a1d61d7bd86e20ac99a38d776ff3abe0
James Cox <jamesncox@yahoo.com>
2019-04-10 20:17:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,24 @@
 class Backer
+<<<<<<< HEAD
 attr_reader :name, :backed_projects
+=======
+attr_accessor :name, :backed_projects
+attr_reader
+>>>>>>> d2b8235c2f5f216b8fbaa6b760e13500d1b61aef
 
 def initialize(name)
   @name = name
   @backed_projects = []
 end
 
+<<<<<<< HEAD
 def back_project(project)
   backed_projects << project
   project.backers << self
+=======
+def backed_projects(title)
+  @backed_projects << title
+>>>>>>> d2b8235c2f5f216b8fbaa6b760e13500d1b61aef
 end
   
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,22 @@
 class Project
+<<<<<<< HEAD
 attr_reader :title, :backers
+=======
+attr_accessor :title, :backers
+attr_reader
+>>>>>>> d2b8235c2f5f216b8fbaa6b760e13500d1b61aef
   
 def initialize(title)
   @title = title
   @backers = []
 end
+<<<<<<< HEAD
 
 def add_backer(backer)
   backers << backer
   backer.backed_projects << self
 end
+=======
+>>>>>>> d2b8235c2f5f216b8fbaa6b760e13500d1b61aef
   
 end
